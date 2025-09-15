1b958c677be689f1d49f0e089aff52efa61fc48d
Aziola <gluskov934@gmail.com>
2025-09-15 18:55:11 +0300

    Заполнение всех трат и их стоимость + разделение по разным массивам

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 //
+int a = 0;
+Console.WriteLine("Сколько операций у вас за сегодня было? (2-40)");
+a = Convert.ToInt32(Console.ReadLine());
+string[] Operations = new string[a];
+int[] Prices = new int[a];
+string request = "";
+string[] words = new string[2];
+for (int i = 0; i < a; i++)
+{
+    request = Console.ReadLine();
+    words = request.Split(new char[] { ';' },
+    StringSplitOptions.RemoveEmptyEntries);
+    words[1] = words[1].Trim(); 
+    Operations[i] = words[0];
+    Prices[i] = Convert.ToInt32(words[1]);
+
+    
+}
