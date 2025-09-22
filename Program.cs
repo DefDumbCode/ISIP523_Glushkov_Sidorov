c2c2ecf4047b1987aba96119b54016874bbfc37b
Aziola <gluskov934@gmail.com>
2025-09-22 16:44:26 +0300

    Создание класса с конструктором без категории

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+class Product
+{
+    public static int Id = 1;
+    public string Name;
+    public int Price;
+    public int Quant;
+
+    public Product(string Name, int Price, int Quant)
+    {
+        Id++;
+        this.Name = Name;
+        this.Price = Price;
+        this.Quant = Quant;
+    }
+}
