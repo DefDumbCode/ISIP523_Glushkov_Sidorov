4ab27eb1c29b78e620205da5c9dd0a44d1214159
Aziola <gluskov934@gmail.com>
2025-09-22 16:57:42 +0300

    Добавил категории, а также менюшку

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+bool a = true;
+while (a)
+{
+    Console.WriteLine("-----МЕНЮ-----");
+    Console.WriteLine("1 - Добавить товар");
+    Console.WriteLine("2 - Удалить товар");
+    Console.WriteLine("3 - Заказать поставку товара");
+    Console.WriteLine("4 - Продать товар");
+    Console.WriteLine("5 - Поиск товара по коду");
+    Console.WriteLine("6 - Поиск товара по названию");
+    Console.WriteLine("7 - Поиск товара по категории");
+    Console.WriteLine("0 - Выход");
+
+
+}
 
 class Product
 {
@@ -7,12 +22,21 @@ class Product
     public string Name;
     public int Price;
     public int Quant;
+    public Category Categ;
 
-    public Product(string Name, int Price, int Quant)
+    public Product(string Name, int Price, int Quant, Category Categ)
     {
         Id++;
         this.Name = Name;
         this.Price = Price;
         this.Quant = Quant;
+        this.Categ = Categ;
     }
+}
+
+enum Category
+{
+    Еда = 1,
+    Оружие,
+    Другое
 }
