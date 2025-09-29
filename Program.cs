277323aa0a6ffd9d2d2de9a1986b6c784c157100
Aziola <gluskov934@gmail.com>
2025-09-29 15:22:53 +0300

    Первые шаги и попытка наработать общую картину проги

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите свой текст: ");
+string text = Console.ReadLine();
+int words = 0;
+int sent = 0;
+string small_word = "";
+string big_word = "";
+foreach(char let in text)
+{
+
+}
