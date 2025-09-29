64abdab2806cc9fba5b9e02e306caa3caa5cad1d
Aziola <gluskov934@gmail.com>
2025-09-29 15:52:54 +0300

    Добавил проверку на кол-во символов и первые функции

--- Program.cs
+++ Program.cs
@@ -1,11 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите свой текст: ");
 string text = Console.ReadLine();
+int count = 0;
 int words = 0;
 int sent = 0;
 string small_word = "";
 string big_word = "";
 foreach(char let in text)
 {
-
+    if(let == ' ')
+    {
+        words++;
+    }
+    if (let == '-')
+    {
+        words--;
+    }
+    if(let == '.' || let == '!' || let == '?')
+    {
+        sent++;
+    }
+    count++;
+    
 }
+words++;
+if (count > 100) {
+    Console.WriteLine(words);
+    Console.WriteLine(sent);
+}
+else
+{
+    Console.WriteLine("В тексте меньше ста символов!");
+}
+
