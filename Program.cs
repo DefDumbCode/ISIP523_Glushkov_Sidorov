5ab1d5b07414f447166679f1c1673a85f5bbc442
Aziola <gluskov934@gmail.com>
2025-10-13 16:11:11 +0300

    Поправил классы + добавил новый с курсами и добавил менюшку

--- Program.cs
+++ Program.cs
@@ -1,6 +1,76 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
 Console.WriteLine("Hello, World!");
 
+List<string> Courses = new List<string>();
+List<string> Names = new List<string>();
+
+bool t = true;
+while (t)
+{
+    Console.WriteLine("----------МЕНЮ----------");
+    Console.WriteLine("1 - Добавить студента");
+    Console.WriteLine("2 - Вывести инфу про студентов");
+    Console.WriteLine("3 - Добавить препода");
+    Console.WriteLine("4 - Вывести инфу про преподов");
+    Console.WriteLine("5 - Добавить курс");
+    Console.WriteLine("6 - Вывести инфу про курсы");
+    Console.WriteLine("7 - Найти по Фамилии курсыыы");
+    Console.WriteLine("8 - ВЫВЕСТИ ВСЁЁЁ");
+    Console.WriteLine("0 - Выход");
+
+    int o = Convert.ToInt32(Console.ReadLine());
+    switch (o)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 0:
+            return;
+    }
+}
+
+class Course
+{
+    private int Id;
+    private string Title;
+    private string Description;
+    private string Teacher;
+    private List<string> Names;
+
+    public Course(int Id, string Title, string Description, string Teacher, List<string> Names)
+    {
+        this.Id = Id;
+        this.Title = Title;
+        this.Description = Description;
+        this.Teacher = Teacher;
+        this.Names = Names;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Title: {Title}\nDescription: {Description}\nTeacher: {Teacher} ");
+        foreach (var be in Names)
+        {
+            Console.WriteLine(be);
+        }
+    }
+
+}
 class Person
 {
     private string FIO;
@@ -22,16 +92,16 @@ class Person
 class Student : Person
 {
     private int StudentNumberID;
-    private bool PCExperience;
-    private int CourseNumber;
+    private int PCExperience;
+    private List<string> Courses;
     private string TheHealthGroup;
 
-    public Student(int id, string fio, DateOnly birthday, string gender, bool PCExperience, string TheHealthGroup, int CourseNumber)
+    public Student(int id, string fio, DateOnly birthday, string gender, int PCExperience, List<string> Courses, string TheHealthGroup)
         : base(fio, birthday, gender) // вызов конструктора Person
     {
         StudentNumberID = id;
         this.PCExperience = PCExperience;
-        this.CourseNumber = CourseNumber;
+        this.Courses = Courses;
         this.TheHealthGroup = TheHealthGroup;
     }
 
@@ -39,28 +109,40 @@ class Student : Person
     {
         Console.WriteLine("Student");
         base.Print();
-        Console.WriteLine($"StudentNumberID: {StudentNumberID}\nPCExperience: {PCExperience}\nTheHealthGroup: {TheHealthGroup}\nCourseNumber: {CourseNumber}\n");
+        Console.WriteLine($"StudentNumberID: {StudentNumberID}\nPCExperience: {PCExperience}\n");
+        Console.WriteLine("Courses:");
+        foreach (var be in Courses)
+        {
+            Console.WriteLine(be);
+        }
+        Console.WriteLine($"TheHealthGroup: {TheHealthGroup}\n");
     }
 }
 
 class Teacher : Person
 {
-    private string Subject;
+    private List<string> Courses;
     private int ExperienceYears;
 
-    public Teacher(string fio, DateOnly birthday, string gender, string subject, int PCExperience)
+    public Teacher(string fio, DateOnly birthday, string gender, List<string> Courses, int WorkExperience)
         : base(fio, birthday, gender)
     {
-        Subject = subject;
-        ExperienceYears = PCExperience;
+        Courses = Courses;
+        ExperienceYears = WorkExperience;
     }
 
     public override void Print()
     {
         Console.WriteLine("Teacher");
         base.Print();
-        Console.WriteLine($"Subject: {Subject}\nExperienceYears: {ExperienceYears}\n");
+        foreach (var be in Courses)
+        {
+            Console.WriteLine(be);
+        }
+        Console.WriteLine($"ExperienceYears: {ExperienceYears}\n");
     }
 }
+
+
 
 
