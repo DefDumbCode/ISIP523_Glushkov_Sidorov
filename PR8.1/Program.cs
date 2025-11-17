cc8d8a93f12b775398c291108fc69f98d553ec9b
Aziola <gluskov934@gmail.com>
2025-11-17 18:23:56 +0300

    устал

--- PR8.1/Program.cs
+++ PR8.1/Program.cs
@@ -26,87 +26,113 @@ namespace PR8._1
                         shopp.Buy(NewUser, In_Acc);
                         break;
                     case 2:
-                       
-                        Console.WriteLine("---КОРЗИНА---");
-
-                        var userProducts = Core.Context.User_Product
-                        .Where(up => up.ID_User == NewUser.ID)
-                        .ToList();
-                        foreach (var u in userProducts)
+                        if (In_Acc == false)
                         {
-                            Console.WriteLine($"ID: {u.ID} Название: {u.Product.Name}, Цена {u.Product.Price * u.Amount}, Кол-во {u.Amount}");
-                        }
-                        Console.WriteLine("Вы хотите купить определенный товар или всю корзину? \n 1) Один \n 2) Всё");
-                        int qqq = Convert.ToInt32(Console.ReadLine());
-                        switch (qqq)
-                        {
-                            case 1:
-                                Console.WriteLine("Введите ID, который вы хотите купить.");
-                                int i = Convert.ToInt32(Console.ReadLine());
-                                UsProd3 = Core.Context.User_Product.FirstOrDefault(u => u.ID == i);
-                                break;
-                            case 2:
-                                break;
-                        }
-                        Console.WriteLine("В какой ПВЗ вы хотите это заказать?");
-                        List<PVZ> pvz = Core.Context.PVZ.ToList();
-                        foreach (var u in pvz)
-                        {
-                            Console.WriteLine($"ID: {u.ID} Адрес: {u.Adress}");
-                        }
-                        Console.WriteLine("Введите ID нужного ПВЗ:");
-                        int ii = Convert.ToInt32(Console.ReadLine());
-                        PVZ pVZ = Core.Context.PVZ.FirstOrDefault(p => p.ID == ii);
-                        Order New_Ord = new Order
-                        {
-                            ID_PVZ = pVZ.ID,
-                            Date = DateTime.Now,
-                            ID_User = NewUser.ID
-                        };
-                        Core.Context.Order.Add(New_Ord);
-                        Core.Context.SaveChanges();
-                        if (qqq == 1)
-                        {
-                            Order_Product Us_Ord = new Order_Product
-                            {
-                                ID_Order = New_Ord.ID,
-                                ID_Product = UsProd3.ID_Product,
-                                Amount = UsProd3.Amount
-                            };
-                            Core.Context.Order_Product.Add(Us_Ord);
-                            Core.Context.User_Product.Remove(UsProd3);
-                            Core.Context.SaveChanges();
+                            Console.WriteLine("ВОЙДИ В АККАУНТ, ИДИОТ");
+                            break;
                         }
                         else
                         {
-                            foreach(var u in userProducts)
+                            Console.WriteLine("---КОРЗИНА---");
+
+                            var userProducts = Core.Context.User_Product
+                            .Where(up => up.ID_User == NewUser.ID)
+                            .ToList();
+                            if (userProducts.Count == 0)
                             {
-                                Order_Product Us_Ord = new Order_Product
+                                Console.WriteLine("Ваша корзина пуста!");
+                                break;
+                            }
+                            else
+                            {
+
+                                foreach (var u in userProducts)
                                 {
-                                    ID_Order = New_Ord.ID,
-                                    ID_Product = u.ID_Product,
-                                    Amount = u.Amount
+                                    Console.WriteLine($"ID: {u.ID} Название: {u.Product.Name}, Цена {u.Product.Price * u.Amount}, Кол-во {u.Amount}");
+                                }
+                                Console.WriteLine("Вы хотите купить определенный товар или всю корзину? \n 1) Один \n 2) Всё");
+                                int qqq = Convert.ToInt32(Console.ReadLine());
+                                switch (qqq)
+                                {
+                                    case 1:
+                                        Console.WriteLine("Введите ID, который вы хотите купить.");
+                                        int i = Convert.ToInt32(Console.ReadLine());
+                                        UsProd3 = Core.Context.User_Product.FirstOrDefault(u => u.ID == i);
+                                        break;
+                                    case 2:
+                                        break;
+                                }
+                                Console.WriteLine("В какой ПВЗ вы хотите это заказать?");
+                                List<PVZ> pvz = Core.Context.PVZ.ToList();
+                                foreach (var u in pvz)
+                                {
+                                    Console.WriteLine($"ID: {u.ID} Адрес: {u.Adress}");
+                                }
+                                Console.WriteLine("Введите ID нужного ПВЗ:");
+                                int ii = Convert.ToInt32(Console.ReadLine());
+                                PVZ pVZ = Core.Context.PVZ.FirstOrDefault(p => p.ID == ii);
+                                Order New_Ord = new Order
+                                {
+                                    ID_PVZ = pVZ.ID,
+                                    Date = DateTime.Now,
+                                    ID_User = NewUser.ID
                                 };
-                                Core.Context.Order_Product.Add(Us_Ord);
-                                Core.Context.User_Product.Remove(u);
+                                Core.Context.Order.Add(New_Ord);
                                 Core.Context.SaveChanges();
+                                if (qqq == 1)
+                                {
+                                    Order_Product Us_Ord = new Order_Product
+                                    {
+                                        ID_Order = New_Ord.ID,
+                                        ID_Product = UsProd3.ID_Product,
+                                        Amount = UsProd3.Amount
+                                    };
+                                    Core.Context.Order_Product.Add(Us_Ord);
+                                    Core.Context.User_Product.Remove(UsProd3);
+                                    Core.Context.SaveChanges();
+                                }
+                                else
+                                {
+                                    foreach (var u in userProducts)
+                                    {
+                                        Order_Product Us_Ord = new Order_Product
+                                        {
+                                            ID_Order = New_Ord.ID,
+                                            ID_Product = u.ID_Product,
+                                            Amount = u.Amount
+                                        };
+                                        Core.Context.Order_Product.Add(Us_Ord);
+                                        Core.Context.User_Product.Remove(u);
+                                        Core.Context.SaveChanges();
+                                    }
+                                }
+                                Console.WriteLine("Заказ оформлен!");
+                                break;
                             }
                         }
-                        Console.WriteLine("Заказ оформлен!");
-                            break;
                     case 3:
-                        var orders = Core.Context.Order.ToList();
-                        foreach (var order in orders)
+                        if (In_Acc == false)
                         {
-                            var Us_Ord2 = Core.Context.Order_Product
-                            .Where(up => up.ID_Order == order.ID && NewUser.ID == order.ID)
-                            .ToList();
-                            foreach (var u in Us_Ord2)
-                            {
-                                var product = Core.Context.Product.FirstOrDefault(p => p.ID == u.ID_Product);
-                                var pvvz = Core.Context.PVZ.FirstOrDefault(pv => pv.ID == order.ID_PVZ);
-                                Console.WriteLine($"Name: {product.Name}, Amount: {u.Amount}, PVZ: {pvvz.Adress}, Date: {order.Date}");
-                            }
+                            Console.WriteLine("ВОЙДИ В АККАУНТ, ИДИОТ");
+                            break;
+                        }
+                        else
+                        {
+                            var orders = Core.Context.Order.Where(o => o.ID_User == NewUser.ID).ToList();
+                            Console.WriteLine("===ЗАКАЗЫ===");
+                                foreach (var order in orders)
+                                {
+
+                                    var Us_Ord2 = order.Order_Product.ToList();
+                                    foreach (var u in Us_Ord2)
+                                    {
+                                        var product = Core.Context.Product.FirstOrDefault(p => p.ID == u.ID_Product);
+                                        var pvvz = Core.Context.PVZ.FirstOrDefault(pv => pv.ID == order.ID_PVZ);
+                                        Console.WriteLine($"Name: {product.Name}, Amount: {u.Amount}, PVZ: {pvvz.Adress}, Date: {order.Date}");
+                                    }
+                                }
+
+                                
                         }
 
                         break;
@@ -171,18 +197,26 @@ namespace PR8._1
                     Console.WriteLine("Введите точное название товара, который вы хотите купить.");
                     string l = Console.ReadLine();
                     Product Prod2 = Core.Context.Product.FirstOrDefault(p => p.Name == l);
-                    Console.WriteLine("Сколько вы хотите купить?");
-                    int x = Convert.ToInt32(Console.ReadLine());
-                    
-                    User_Product New_Us_Prod = new User_Product
+                    if(Prod2 == null)
                     {
-                        ID_User = NewUser.ID,
-                        ID_Product = Prod2.ID,
-                        Amount = x
-                    };
-                    Console.WriteLine($"Вы добавили в корзину {l} единиц предмета {x}");
-                    Core.Context.User_Product.Add(New_Us_Prod);
-                    Core.Context.SaveChanges();
+                        Console.WriteLine("Название неправильное!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Сколько вы хотите купить?");
+                        int x = Convert.ToInt32(Console.ReadLine());
+
+                        User_Product New_Us_Prod = new User_Product
+                        {
+                            ID_User = NewUser.ID,
+                            ID_Product = Prod2.ID,
+                            Amount = x
+                        };
+                        Console.WriteLine($"Вы добавили в корзину {l} единиц предмета {x}");
+                        Core.Context.User_Product.Add(New_Us_Prod);
+                        Core.Context.SaveChanges();
+                    }
+                        
                 }
                 
             }
