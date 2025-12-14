a43e4da30e5ba1a3edf9d243ae16c84c3bd65328
Aziola <gluskov934@gmail.com>
2025-12-14 23:00:33 +0300

    Добавил броню

--- Game.cs
+++ Game.cs
@@ -9,5 +9,10 @@ namespace ISIP523_Glushkov
     internal class Game
     {
         int count = 0;
+
+        public void Start_game()
+        {
+
+        }
     }
 }
---
+++ Model/Items/Armor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP523_Glushkov.Model.Items
+{
+    public class Armor
+    {
+        public string Name;
+        public double Damage;
+
+        public Armor(string name, double damage)
+        {
+            Name = name;
+            Damage = damage;
+
+        }
+
+        public virtual string GetArmorInfo()
+        {
+            return $"{Name} (Урон: {Damage})";
+        }
+
+    }
+
+    public class Bum : Armor
+    {
+        public Bum()
+            : base("Обноски бездомного", 0.20) { }
+
+    }
+
+    public class Thief : Armor
+    {
+        public Thief()
+            : base("Одежка вора", 0.25) { }
+
+    }
+
+    public class Ranger : Armor
+    {
+        public Ranger()
+            : base("Обмундирование Рейнджера", 0.40) { }
+
+    }
+    public class Knight : Armor
+    {
+        public Knight()
+            : base("Броня рыцаря", 0.60) { }
+
+    }
+
+    public class Banana : Armor
+    {
+        public Banana()
+            : base("Костюм банана", 0.85) { }
+
+    }
+}
+}
